4d8dfe9b2accb06222e2cc88ddc77eb87b86a889
SuryankDixit <suryankdixit@gmail.com>
2020-06-05 11:36:51 +0530

    Modified

--- glthreads/glthread_declarations.h
+++ glthreads/glthread_declarations.h
@@ -1,3 +1,5 @@
+//#ifndef __A__
+//#define __A__
 
 // Structue for glued doubly linked list;
 
@@ -21,12 +23,20 @@ typedef struct glthread{
 	link->left = NULL; 		\
 	link->right = NULL;
 	
-#define ITERATE_LIST(list_ptr,struct_type,s)	\
+/*#define ITERATE_LIST(list_ptr,struct_type,s)	\
 	node *temp=NULL;		\
 	for(temp=list_ptr->head;temp!=NULL;temp=temp->right){	\
 		s=(struct_type *)((char *)temp-list_ptr->offset);	\
 		print_details(s);	\
-		}		\
+		}	*/
+		
+#define ITERATE_GL_THREADS_BEGIN(lstptr, struct_type, s)      \
+{                                                               \
+    node *_glnode = NULL;             \
+    for(_glnode = lstptr->head; _glnode!=NULL; _glnode = _glnode->right){      \
+        s = (struct_type *)((char *)_glnode - lstptr->offset);
+
+#define ITERATE_GL_THREADS_ENDS }}	
 
 
 //function declarations
@@ -36,4 +46,4 @@ void list_insert(list *list_ptr,node *ptr);
 void print_list(list *list_ptr);
 void list_delete(list *list_ptr,node *ptr);
 
-
+//#endif
