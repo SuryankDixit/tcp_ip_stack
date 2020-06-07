05d7906f152dfe686e0b2858dc318ee802d4af9d
SuryankDixit <suryankdixit@gmail.com>
2020-06-07 11:21:55 +0530

    First Phase Complete: Generic Graph

--- gluethread/glthread.c
+++ gluethread/glthread.c
@@ -1,1 +1,29 @@
+#include "glthread.h"
+#include <stdio.h>
+#include <memory.h>
+#include <string.h>
+#include <stdlib.h>
 
+void initialize_glthread(glthread_t *glthread)
+{
+	glthread->left=NULL;
+	glthread->right=NULL;
+}
+
+void add_node_front(glthread_t *head, glthread_t *node)
+{
+	if(head->right==NULL)
+	{
+		head->right=node;
+		node->left=head;
+		return;
+	}
+	
+	glthread_t *temp=NULL;
+	temp=head->right;
+	head->right=node;
+	node->left=head;
+	node->right=temp;
+	temp->left=node;
+	return;
+}
