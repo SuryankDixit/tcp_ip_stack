e6d324d9f95eaae8442ca6bc82c318536a1dcc8e
SuryankDixit <suryankdixit@gmail.com>
2020-06-21 14:34:05 +0530

    modified

--- topologies.c
+++ topologies.c
@@ -51,6 +51,31 @@ graph_t *build_first_topo()
     	node_set_interface_ip_address(node_R2, "eth0/5", "40.1.1.2", 24);
     	
     	network_start_pkt_receiver_thread(topo);	// this function starts listening on the UDP-Sockets.
+	return topo;
+}
+
+
+graph_t *build_linear_topo()
+{
+	graph_t *topo= create_new_graph("Linear Topology");  	// Graph creation
 	
+	node_t *H1=create_graph_node(topo , "H1");		// Creating and adding nodes in the graph
+	node_t *H2=create_graph_node(topo , "H2");
+	node_t *H3=create_graph_node(topo , "H3");
+	
+	insert_link_bw_two_nodes(H1,H2,"eth0/1","eth0/2",1);
+	insert_link_bw_two_nodes(H2,H3,"eth0/3","eth0/4",1);
+	
+	node_set_loopback_address(H1,"122.1.1.0");
+	node_set_interface_ip_address(H1, "eth0/1", "10.1.1.1", 24);
+    	
+    	node_set_loopback_address(H2, "122.1.1.1");
+   	node_set_interface_ip_address(H2, "eth0/2", "10.1.1.2", 24);
+        node_set_interface_ip_address(H2, "eth0/3", "20.1.1.2", 24);
+        
+        node_set_loopback_address(H3, "122.1.1.2");
+    	node_set_interface_ip_address(H3, "eth0/4", "20.1.1.1", 24);
+    	
+    	network_start_pkt_receiver_thread(topo);	// this function starts listening on the UDP-Sockets.
 	return topo;
 }
