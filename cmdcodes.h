d264aac3f92701808ea65308565ace0b33d547a9
SuryankDixit <suryankdixit@gmail.com>
2020-06-21 14:32:20 +0530

    modified

--- cmdcodes.h
+++ cmdcodes.h
@@ -2,6 +2,15 @@
 #define __CMDCODES__
 
 #define CMDCODE_SHOW_NETWORK_TOPOLOGY    1   /*show topology*/
+#define CMDCODE_PING                2   /*run <node-name> ping <ip-address>*/
+#define CMDCODE_SHOW_NODE_ARP_TABLE 3   /*show node <node-name> arp*/
+#define CMDCODE_RUN_ARP             4   /*run node <node-name> resolve-arp <ip-address>*/
+#define CMDCODE_INTF_CONFIG_L2_MODE 5   /*config node <node-name> interface <intf-name> l2mode <access|trunk>*/
+#define CMDCODE_INTF_CONFIG_IP_ADDR 6   /*config node <node-name> interface <intf-name> ip-address <ip-address> <mask>*/
+#define CMDCODE_INTF_CONFIG_VLAN    7   /*config node <node-name> interface <intf-name> vlan <vlan-id>*/
+#define CMDCODE_SHOW_NODE_MAC_TABLE 8   /*show node <node-name> mac*/
+#define CMDCODE_SHOW_NODE_RT_TABLE  9   /*show node <node-name> rt*/
+#define CMDCODE_CONF_NODE_L3ROUTE   10  /*config node <node-name> route <ip-address> <mask> [<gw-ip> <oif>]*/
 
 
 #endif
