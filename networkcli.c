82042a5362e83a6c4afc6d33214d114db4711d3c
SuryankDixit <suryankdixit@gmail.com>
2020-06-21 14:34:00 +0530

    modified

--- networkcli.c
+++ networkcli.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 extern graph_t *topo; 
+
+/*General Validations*/
+int
+validate_node_extistence(char *node_name){
+
+    node_t *node = get_node_by_node_name(topo, node_name);
+    if(node)
+        return VALIDATION_SUCCESS;
+    printf("Error : Node %s do not exist\n", node_name);
+    return VALIDATION_FAILED;
+}
+
+
 // Second arguement is the pointer to the buffer, which will hold the leaf param values passed by user.
 
 int show_network_topology_handler(param_t *param, 
@@ -21,6 +34,57 @@ int show_network_topology_handler(param_t *param,
             					break;
         default: ;
     	}
+    return 0;
+}
+
+typedef struct arp_table_ arp_table_t;
+extern void
+print_arp_table(arp_table_t *arp_table);
+
+static int show_arp_handler(param_t *param, ser_buff_t *tlv_buf, 
+                    op_mode enable_or_disable){
+
+    node_t *node;
+    char *node_name;
+    tlv_struct_t *tlv = NULL;
+    
+    TLV_LOOP_BEGIN(tlv_buf, tlv){
+
+        if(strncmp(tlv->leaf_id, "node-name", strlen("node-name")) ==0)
+            node_name = tlv->value;
+
+    }TLV_LOOP_END;
+
+    node = get_node_by_node_name(topo, node_name);
+    print_arp_table(node->node_nw_prop.arp_table);    
+    return 0;
+}
+
+
+extern void
+send_arp_broadcast_request(node_t *node,
+                           interface_t *oif,
+                           char *ip_addr);
+static int
+arp_handler(param_t *param, ser_buff_t *tlv_buf,
+                op_mode enable_or_disable){
+
+    node_t *node;
+    char *node_name;
+    char *ip_addr;
+    tlv_struct_t *tlv = NULL;
+
+    TLV_LOOP_BEGIN(tlv_buf, tlv){
+
+        if(strncmp(tlv->leaf_id, "node-name", strlen("node-name")) ==0)
+            node_name = tlv->value;
+        else if(strncmp(tlv->leaf_id, "ip-address", strlen("ip-address")) ==0)
+            ip_addr = tlv->value;
+    } TLV_LOOP_END;
+
+    node = get_node_by_node_name(topo, node_name);
+    //printf("HII-CLI");
+    send_arp_broadcast_request(node, NULL, ip_addr);
     return 0;
 }
 
@@ -63,5 +127,61 @@ void network_init_cli()
              * are expected leaf params we need to parse.*/
 								
 		set_param_cmd_code(&topology, CMDCODE_SHOW_NETWORK_TOPOLOGY);
+		
+		{
+		    /*show node*/    
+		     static param_t node;
+		     init_param(&node, CMD, "node", 0, 0, INVALID, 0, "\"node\" keyword");
+		     libcli_register_param(show, &node);
+		     //libcli_register_display_callback(&node, display_graph_nodes);
+		     {
+		        /*show node <node-name>*/ 
+		         static param_t node_name;
+		         init_param(&node_name, LEAF, 0, 0, validate_node_extistence, STRING, "node-name", "Node Name");
+		         libcli_register_param(&node, &node_name);
+		         {
+		            /*show node <node-name> arp*/
+		            static param_t arp;
+		            init_param(&arp, CMD, "arp", show_arp_handler, 0, INVALID, 0, "\"arp\" keyword");
+		            libcli_register_param(&node_name, &arp);
+		            set_param_cmd_code(&arp, CMDCODE_SHOW_NODE_ARP_TABLE);
+		         }
+		           
+		     }
+		 } 
 	}
+	
+	
+	    {
+        /*run node*/
+        static param_t node;
+        init_param(&node, CMD, "node", 0, 0, INVALID, 0, "\"node\" keyword");
+        libcli_register_param(run, &node);
+        //libcli_register_display_callback(&node, display_graph_nodes);
+        {
+            /*run node <node-name>*/
+            static param_t node_name;
+            init_param(&node_name, LEAF, 0, 0, validate_node_extistence, STRING, "node-name", "Node Name");
+            libcli_register_param(&node, &node_name);
+            {
+                /*run node <node-name> ping */
+                static param_t ping;
+                init_param(&ping, CMD, "ping" , 0, 0, INVALID, 0, "Ping utility");
+                libcli_register_param(&node_name, &ping);
+            }
+            {
+                /*run node <node-name> resolve-arp*/    
+                static param_t resolve_arp;
+                init_param(&resolve_arp, CMD, "resolve-arp", 0, 0, INVALID, 0, "Resolve ARP");
+                libcli_register_param(&node_name, &resolve_arp);
+                {
+                    /*run node <node-name> resolve-arp <ip-address>*/    
+                    static param_t ip_addr;
+                    init_param(&ip_addr, LEAF, 0, arp_handler, 0, IPV4, "ip-address", "Nbr IPv4 Address");
+                    libcli_register_param(&resolve_arp, &ip_addr);
+                    set_param_cmd_code(&ip_addr, CMDCODE_RUN_ARP);
+                }
+            }
+        }
+    }
 }
